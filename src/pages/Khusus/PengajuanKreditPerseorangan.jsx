aefd27a03a9e85071f8fe543dd752c0de655c86e
paulinariska <paulinariskaoktaviani@gmail.com>
2024-05-10 10:44:02 +0700

    Add StepNavigation

--- src/pages/Khusus/PengajuanKreditPerseorangan.jsx
+++ src/pages/Khusus/PengajuanKreditPerseorangan.jsx
@@ -1,41 +1,19 @@
 import { useState } from "react";
 import Button from "../../components/Button";
 import FormDataDiri from "../../components/Form/FormPengajuanKreditPerseorangan";
+import StepNavigation from "../../components/StepNavigation";
 
 
 function PengajuanKreditPerseorangan(){
 
-    <div className="flex justify-between">
-        <div>
-            <Button>
-                Data Diri
-            </Button>
-        </div>
-        <div>
-            <Button>
-                Berkas Diri & Kontak Darurat
-            </Button>
-        </div>
-        <div>
-            <Button>
-                Data Usaha
-            </Button>
-        </div>
-        <div>
-            <Button>
-                Data Usaha 2
-            </Button>
-        </div>
-        <div>
-            <Button>
-                Pengajuan
-            </Button>
-        </div>
-    </div>
-
     return (
         <>
-            <FormDataDiri/>
+            <div>
+                <StepNavigation/>
+            </div>
+            <div>
+                <FormDataDiri/>
+            </div>  
         </>
     )
 
