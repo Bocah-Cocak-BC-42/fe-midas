edfd5943f797c57c8dc4b8fc53677f5b25449394
paulinariska <paulinariskaoktaviani@gmail.com>
2024-05-10 11:05:28 +0700

    revise function

--- src/pages/Khusus/PengajuanKreditPerseorangan.jsx
+++ src/pages/Khusus/PengajuanKreditPerseorangan.jsx
@@ -6,18 +6,20 @@ import StepNavigation from "../../components/StepNavigation";
 
 function PengajuanKreditPerseorangan(){
 
+    const [tab, setTab] = useState(1);
+
+    const stepOptions = ["Data Diri", "Berkas Diri & Kontak Darurat", "Data Usaha", "Pengajuan"];
     return (
         <>
             <div>
-                <StepNavigation/>
+                <StepNavigation stepOptions={stepOptions} tab={tab}/>
             </div>
             <div>
                 <FormDataDiri/>
             </div>  
         </>
     )
-
-
+    
 }
 
 
